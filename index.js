8da81c416252c36299d20c66cd19c3b884cb2c50
Sophistiqq <roi.for.school@gmail.com>
2024-05-11 18:43:45 +0800

    Fixed Status 500

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const app = express();
-import connection from './dbconfig.js'
+import './dbconfig.js'
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index.ejs')
 })
 
 
---
+++ testing.js
@@ -0,0 +1,6 @@
+import app from './index.js';
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+})
