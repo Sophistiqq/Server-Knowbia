33a0ce73314f6083be12632fbf79db9656d55944
Sophistiqq <roi.for.school@gmail.com>
2024-05-11 18:12:13 +0800

    Fixed server crash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-import connection from './dbconfig'
+import connection from './dbconfig.js'
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -8,6 +8,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World'})
 })
+
 
 
 console.log("Hello")
